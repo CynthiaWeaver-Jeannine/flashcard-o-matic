85a0d216b0d5b9afa06cf2d0ee810f8ef4adb34b
cynthiajweaverdev <132491873+CynthiaWeaver-Jeannine@users.noreply.github.com>
2023-06-03 13:46:35 -0400

    add comments

--- src/Layout/DeckView/MapCards.js
+++ src/Layout/DeckView/MapCards.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ViewCards from "./ViewCards";
 
+/* use .map() to iterate over the cards array and create a new array of components
+for each element in the cards array, the .map() is called with a callback function
+The .map() method returns a new array of the ViewCards components
+which is then rendered as the output */
 function MapCards({ cards, cardDeleteHandler, deckId }) {
   return cards.map((card, index) => {
     return (
